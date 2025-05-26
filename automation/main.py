9ce80143120e310198e1630d5516783fb04436c9
Hazel Santos <48263625+reddax02@users.noreply.github.com>
2025-05-26 10:25:37 -0400

    Imported dependencies main.py
    Dependencies in the file script

--- automation/main.py
+++ automation/main.py
@@ -1,1 +1,12 @@
-print("Hello from GitHub!")
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+import json
+import time
+import random
+import csv
+from datetime import datetime

c1dd2e2ab13b8847b4ce2561043297a7252dd378
Hazel Santos <48263625+reddax02@users.noreply.github.com>
2025-05-26 16:32:37 -0400

    Update automationmain.py
    go directly to jobs page

--- automation/automationmain.py
+++ automation/automationmain.py
@@ -11,10 +11,25 @@ import random
 import csv
 from datetime import datetime
 
-class EasyApplyLinkedIn:
-  def __init__(self,data):
-    ""Parameter Initizalization""
-    self.email = data['password']
-    self.password = data['keywords']
-    self.location = data['location']
-    self.driver = webdriver.Chrome(service=chrome_service)
+class EasyApplyLinkedin:
+    def __init__(self, data):
+        """Parameter Initialization"""
+        self.email = data['email']
+        self.password = data['password']
+        self.keywords = data['keywords']
+        self.location = data['location']
+        chrome_service = Service(data['driver_path'])
+        self.driver = webdriver.Chrome(service=chrome_service)
+
+    def login_linkedin(self):
+        '''Login to linkedin profile and go to jobs page after 2 seconds'''
+        self.driver.get("https://www.linkedin.com/login")
+        login_email = self.driver.find_element(By.NAME, "session_key")
+        login_email.clear()
+        login_email.send_keys(self.email)
+        login_password = self.driver.find_element(By.NAME, "session_password")
+        login_password.clear()
+        login_password.send_keys(self.password)
+        login_password.send_keys(Keys.RETURN)
+        time.sleep(2)  # Wait 2 seconds before navigating to Jobs page
+        self.driver.get("https://www.linkedin.com/jobs/")  # Go directly to Jobs page

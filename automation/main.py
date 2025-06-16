6aeb5070b763b153a533108a661894dc32310259
Hazel Santos <48263625+reddax02@users.noreply.github.com>
2025-06-16 10:29:59 -0400

    Changes on dependencies main.py
    updated webdriver_manager

--- automation/main.py
+++ automation/main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
 import json
 import time
 import random

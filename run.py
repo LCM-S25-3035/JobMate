b00a8409c1da853e5ea9ce5dcbfa48f5d1f6198a
Mugilmithran Kathiravan <kathiravanmugilmithran@gmail.com>
2025-08-12 01:48:54 -0400

    fix: internal sever error

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from app import create_app
 
 app = create_app()
 
+# Add SQLAlchemy config here
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 if __name__ == '__main__':
     # Use PORT from environment (Render sets this) or default to 5002
     port = int(os.environ.get('PORT', 5002))
